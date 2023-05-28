f70c2c022e975f764e867671dccaa32646592b1d
Felix Habert <felix.habert.20@ucl.ac.uk>
2023-05-28 11:43:04 +0200

    initiliazed git

---
+++ main.py
@@ -0,0 +1,61 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import GLPNFeatureExtractor, GLPNForDepthEstimation
+from transformers import pipeline
+import os
+import json
+import requests
+from numpy.linalg import norm
+import numpy as np
+
+TOKEN = "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"
+URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/"
+HEADERS = {"Accept": "Application/json", "Authorization": f"Bearer {TOKEN}"}
+
+def openai_embedding():
+    openai_api = os.getenv("OPENAI_API_KEY")
+    print(openai_api)
+    url_embedded = "https://api.openai.com/v1/embeddings"
+    headers = {"Accept": "Application/json","Authorization": "Bearer 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"}
+    payload = { "input": ["I want this to be embedded"],"model": "text-embedding-ada-002","options":{"wait_for_model":True}}
+    web = requests.post(url_embedded, headers=headers, params=json.dumps(payload), auth=None)
+    print("Status:", web.status_code)
+    print(web.text)
+    pass
+
+def cosine_similarity(v1: np.array, v2: np.array) -> np.float64:
+    num = np.dot(v1, v2)
+    den = norm(v1)*norm(v2)
+    return num/den
+
+def total_cos_similarity(v: np.array) -> np.float64:
+    l = len(v)
+    sim_matrix = np.zeros((l, l))
+    total_sim = 0
+    for i in range(l):
+        for j in range(l):
+            sim = cosine_similarity(v[i][0][0], v[j][0][0])
+            sim_matrix[i][j] = sim
+            if i != j:
+                total_sim += sim
+    mean_sim = total_sim / (l*l-l)
+    return mean_sim
+
+def hf_embedding():
+    model_id = "sentence-transformers/all-MiniLM-L6-v2" #direct embedings in english
+    api_url = URL + model_id
+    query = ["football"]
+    response = requests.post(api_url, headers=HEADERS, json={"inputs": query, "options":{"wait_for_model":True}})
+    return response.json()
+
+def similarity():
+    model_dif_languages = "xlm-mlm-enfr-1024"
+    api_url = URL + model_dif_languages
+    query = ["plante", "fleur", "flower"]
+    response = requests.post(api_url, headers=HEADERS, json={"inputs": query, "options":{"wait_for_model":True}})
+    formatted_output = np.array(response.json(), dtype=object)
+    output = total_cos_similarity(np.array(formatted_output))
+    return output
+
+# call_openai()
+embedded_output = similarity()
+print(embedded_output)
---
+++ vectors.py
@@ -0,0 +1,68 @@
+import os
+import openai
+import pinecone
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+PINECONE_API_KEY = "c1bf3fa4-58a9-4d15-86cc-51eed89b1c32"
+PINECONE_ENV = "us-west4-gcp-free"
+PINECONE_TABLE_NAME = "my_test"
+OPENAI_KEY = "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"
+
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
+openai.api_key = OPENAI_KEY
+
+
+
+def create_pinecone_index(table_name, dimension=1536, metric="cosine", pod_type="p1"):
+    if table_name not in pinecone.list_indexes():
+        pinecone.create_index(table_name, dimension=dimension, metric=metric, pod_type=pod_type)
+
+def complete(prompt):
+    res = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return res['choices'][0]['message']['content'].strip()
+
+def get_ada_embedding(text):
+    text = text.replace("\n", " ")
+    print("hey")
+    return openai.Embedding.create(input=text, model="text-embedding-ada-002")["data"][0]["embedding"]
+
+def upsert_to_index(index, texts):
+    pinecone_vectors = []
+    for loopIndex, text in enumerate(texts, start=1):
+        pinecone_vectors.append(("test-openai-"+str(loopIndex), get_ada_embedding(text), {"text": text}))
+    index.upsert(vectors=pinecone_vectors)
+
+def query_index(index, query_text, top_k=3):
+    q_embedding = get_ada_embedding(query_text)
+    pineQ = index.query(q_embedding, top_k=top_k, include_values=False, include_metadata=True)
+    return pineQ
+
+def print_results(pineQ):
+    print(f"\033[36m" + str(pineQ) + "\033[0m")
+    print("\n")
+    for match in pineQ.matches:
+        print(f"\033[1m\033[32m" + match.metadata['text'])
+
+def main():
+    create_pinecone_index(PINECONE_TABLE_NAME)
+    index = pinecone.Index(PINECONE_TABLE_NAME)
+
+    """ 
+    texts = [
+        "AI Agents as virtual employees are the future",
+        "Vector Databases are the future",
+        "AGI is not here....yet."
+    ] 
+    
+    upsert_to_index(index, texts)
+    """
+
+    # query_text = "are vector dbs the future?"
+    # results = query_index(index, query_text)
+    # print_results(results)
+    pinecone.deinit()
+
+main()
